4bb1d7ce0c99e31d246b5be396ffd3776a35a351
Danyal Mahmood <dmahmood@mail.uoguelph.ca>
2020-02-14 16:02:55 -0500

    update hdd process

--- CIS3110_A2_2020/dispatcher.c
+++ CIS3110_A2_2020/dispatcher.c
@@ -23,18 +23,14 @@ typedef struct process{
     int pid;
     int start_time;
     int readystart;
-    int readyend;
     int readytotal;
     int blockedstart;
-    int blockedend;
     int blockedtotal;
     int blockedCount;
     int run_time;
     int finishedCount;
     int exchangeTimes[100];
     int runningtime;
-    int blockedtime;
-    int readytime;
 } Process;
 
 typedef struct cpu {
@@ -85,7 +81,7 @@ void dispatcher(FILE *fd, int harddrive){
     hdd.isRunning = false;
     
     //Process simulation input line by line
-    while (fgets(line_buffer, MAX_LINE_LENGTH, fd) != NULL && line_buffer[0] != '\n'){
+    while (fgets(line_buffer, MAX_LINE_LENGTH, fd) != NULL && line_buffer[0] != '\n') {
         token = strtok(line_buffer, " ");
         sscanf(token, "%d",&start_time);
         processes[processCounter].start_time = start_time;
@@ -100,11 +96,12 @@ void dispatcher(FILE *fd, int harddrive){
         
         num_exchanges = 0;
         token = strtok(NULL, " ");
-        while ( token != NULL ){
+        while ( token != NULL ) {
             processes[processCounter].exchangeTimes[num_exchanges] = exchange_time;
             num_exchanges += sscanf(token, "%d",&exchange_time);
             token = strtok(NULL, " ");
         }
+
        printf("Process %3d wants to start at %6dms and run for %6dms and has %3d hard drive exchanges\n",  process_id, start_time, run_time, num_exchanges);  
        queueInsert(newQueue, &processes[processCounter], processes[processCounter].start_time);
        processCounter++;  
@@ -112,20 +109,17 @@ void dispatcher(FILE *fd, int harddrive){
 
     while(1) {
         while(1) {
-            if (queueisEmpty(newQueue) == 0)
-            {
+            if (queueisEmpty(newQueue) == 0) {
                 break;
             }
 
             Process* peakProcess = (Process*)queuePeak(newQueue);
-            if (peakProcess && peakProcess->start_time == cpu.time)
-            {
+            if (peakProcess && peakProcess->start_time == cpu.time) {
                 queuePop(newQueue);
 
                 int remainingtime = peakProcess->run_time - peakProcess->runningtime;
 
-                if (remainingtime > peakProcess->run_time)
-                {
+                if (remainingtime > peakProcess->run_time) {
                     cpu.process->readystart = cpu.time;
                     queueInsert(readyQueue, cpu.process, remainingtime);
                     cpu.process = peakProcess;
@@ -139,11 +133,9 @@ void dispatcher(FILE *fd, int harddrive){
         }
 
 
-        if (cpu.process->pid == 0 && queueisEmpty(readyQueue) != 0)
-        {
+        if (cpu.process->pid == 0 && queueisEmpty(readyQueue) != 0) {
             cpu.process = (Process*)queuePeak(readyQueue);
-            cpu.process->readyend = cpu.time;
-            cpu.process->readytotal += cpu.process->readyend - cpu.process->readystart;
+            cpu.process->readytotal += cpu.time - cpu.process->readystart;
             queuePop(readyQueue);
         } else if (cpu.process->pid != 0 && cpu.process->run_time == cpu.process->runningtime) {
             finishedRunning++;
@@ -153,56 +145,70 @@ void dispatcher(FILE *fd, int harddrive){
                 cpu.process = &zeroProcess;
             } else {
                 cpu.process = (Process*)queuePeak(readyQueue);
-                cpu.process->readyend = cpu.time;
-                cpu.process->readytotal += cpu.process->readyend - cpu.process->readystart;
+                cpu.process->readytotal += cpu.time - cpu.process->readystart;
                 queuePop(readyQueue);
             }
         }
 
-        // if (hdd.isRunning) {
-        //     hdd.time--;
-        //     if (hdd.time == 0)
-        //     {
-        //         int remainingtime = hdd.process.runningtime - hdd.process.run_time;
-        //         int currentrunningtime = cpu.process.runningtime - cpu.process.run_time;
-        //         if (remainingtime < currentrunningtime)
-        //         {
-        //             queueInsert(readyQueue, &cpu.process, currentrunningtime);
-        //             cpu.process = hdd.process;
-        //         } else {
-        //             queueInsert(readyQueue, &hdd.process, remainingtime);
-        //         }
-        //     }
-        // }
-        
-        // if (cpu.process.exchangeTimes[0] <= cpu.time && !hdd.isRunning)
-        // {
-        //     cpu.process.blockedstart = cpu.process.start_time;
-        //     queueInsert(blockedQueue, &cpu.process, blockedCount);
-        //     blockedCount++;
-        //     hdd.process = cpu.process;
-        //     hdd.isRunning = true;
-        // }
+        if (cpu.process->exchangeTimes[cpu.process->blockedCount] != 0 && cpu.process->exchangeTimes[cpu.process->blockedCount] <= cpu.time) {
+            cpu.process->blockedstart = cpu.time;
+            queueInsert(blockedQueue, &cpu.process, blockedCount);
+            blockedCount++;
+            cpu.process->blockedCount++;
 
+            if (queueisEmpty(readyQueue) != 0) {
+                cpu.process = (Process*)queuePeak(readyQueue);
+                cpu.process->readytotal += cpu.time - cpu.process->readystart;
+                queuePop(readyQueue);
+            } else {
+                cpu.process = &zeroProcess;
+            }
+        }
 
-        if (queueisEmpty(newQueue) == 0 && queueisEmpty(readyQueue) == 0 && cpu.process->pid == 0)
-        {
+        if (hdd.isRunning) {
+            hdd.time--;
+            if (hdd.time == 0) {
+                int remainingtime = hdd.process->run_time - hdd.process->runningtime;
+                int currentrunningtime = cpu.process->run_time - cpu.process->runningtime;
+                if (remainingtime < currentrunningtime) {
+                    queueInsert(readyQueue, cpu.process, currentrunningtime);
+                    cpu.process = hdd.process;
+                } else {
+                    queueInsert(readyQueue, hdd.process, remainingtime);
+                }
+
+                hdd.time = harddrive;
+
+                if (queueisEmpty(blockedQueue) == 0) {
+                    hdd.isRunning = false;
+                } else {
+                    hdd.process = (Process*)queuePeak(blockedQueue);
+                    hdd.process->blockedtotal += cpu.time - hdd.process->blockedstart;
+                    queuePop(blockedQueue);
+                }
+            }
+        } else {
+            if (queueisEmpty(blockedQueue) != 0) {
+                hdd.process = (Process*)queuePeak(blockedQueue);
+                hdd.process->blockedtotal += cpu.time - hdd.process->blockedstart;
+                hdd.isRunning = true;
+                queuePop(blockedQueue);
+            }
+        }
+
+        if (queueisEmpty(newQueue) == 0 && queueisEmpty(blockedQueue) == 0 && queueisEmpty(readyQueue) == 0 && cpu.process->pid == 0) {
             break;
         }
 
         cpu.process->runningtime++;
         cpu.time++;
-
     }
 
     printf("0 %d\n", zeroProcess.runningtime);
 
-    for (int j = 0; j < processCounter; ++j)
-    {
-        for (int i = 0; i < processCounter; ++i)
-        {
-            if (processes[i].finishedCount == j+1)
-            {
+    for (int j = 0; j < processCounter; ++j) {
+        for (int i = 0; i < processCounter; ++i) {
+            if (processes[i].finishedCount == j+1) {
                 printf("%d %d %d %d\n", processes[i].pid, processes[i].runningtime, processes[i].readytotal, processes[i].blockedtotal);
             }
         }
